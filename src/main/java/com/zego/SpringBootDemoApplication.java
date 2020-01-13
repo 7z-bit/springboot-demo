c3eb5f2e699981d43a0649814d93dc8acf53a486
qizhiwen <1970874980@qq.com>
2020-01-13 18:34:35 +0800

    修改入口文件名

--- src/main/java/com/zego/SpringBootDemoApplication.java
+++ src/main/java/com/zego/SpringBootDemoApplication.java
@@ -1,16 +1,15 @@
 package com.zego;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
-public class ZegoMptRechargeApplication {
+public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZegoMptRechargeApplication.class, args);
+        SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
 }
